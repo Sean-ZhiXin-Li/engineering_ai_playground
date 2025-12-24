1a36e33668a3918a4fc0caa76edb0892bfdf1e9f
Sean-ZhiXin-Li <tlizxin209625@gmail.com>
2025-12-24 15:08:55 +0800

    day0: first runnable python script with input and branching

---
+++ test.py
@@ -0,0 +1,14 @@
+param =float( input())
+input(param)
+a = 12
+b = 123
+c = 135
+if param > 1:
+	print(param - 1)
+else:
+	print(abs(param -1))
+if a + b == c:
+	print("Hello Engineering World!!!")
+else:
+	print("Well, you need to check your numbers, ensure you can into a real engineering world")
+
