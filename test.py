bdd6a252a151016880c0f4d6066a4366bb88c65a
Sean-ZhiXin-Li <tlizxin209625@gmail.com>
2025-12-26 15:16:51 +0800

    day2: write metrics.jsom ouput

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+import json
+from datetime import datetime
+
+passed = False
 param =float( input())
 a = 12
 b = 123
 c = 135
 if param > 1:
 	print(param - 1)
+	passed = True
 else:
 	data = (abs(param - 1))
 	with open('Output.txt', 'w') as f:
@@ -13,3 +18,14 @@ if a + b == c:
 else:
 	print("Well, you need to check your numbers, ensure you can into a real engineering world")
 
+timestamp = datetime.now().isoformat()
+metrics ={
+	"param": param,
+	"value": abs(param - 1),
+	"passed": passed,
+	"timestamp": timestamp,
+	"bad": [1, 2, 3]
+}
+
+with open("metrics.json", 'w') as f:
+	json.dump(metrics, f, indent = 2)
